70c31a83cb74233dfbc31c30988b7562ea869291
AdamHearstC22370983 <124101593+AdamHearstC22370983@users.noreply.github.com>
2023-05-01 17:04:16 +0000

    Finalised Bash Script (just need history to work)

--- Bash_Script/Bash_Menu.sh
+++ Bash_Script/Bash_Menu.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
+#exports the bash script so that it can be executed from any directory
+export PATH = "$PATH:/workspaces/OS-YR-1/Bash_Script"
 
+set -o history
 #creating a bash script to allow the user to execute multiple commands
 while true
 do
+    #here is our menu for the user
+    echo " " #gives space between the menus that are created while in the loop
+    echo "----------Bash Script----------"
     echo "1. List all files in current directory"
     echo "2. Show free disk space"
     echo "3. Show system path"
     echo "4. Display command history"
     echo "5. Backup Files"
     echo "6. Exit"
-    read -p "Please choose from the following options: " option
+    echo "--------------------------------"
+    read -p "Please choose from the following options, 1-6: " option #this will take in user input for our option variable
 
     case $option in
         1)
             echo -e "Listing all files in current directory"
-            ls;;
+            ls;; #lists all files in the current directory
         2)
             echo -e "Showing free disk space"
-            df -h;;
+            df -h;; #shows all used and unused disk space
         3)
-            echo -e "System Path: $PATH";;
+            echo -e "System Path: $PATH";; #shows the full current system path
         4)
-            echo -e "Displaying command history"
-            cat ~/.bash_history;;
+            echo -e "Displaying command history: "
+            history;; #shows the command history
         5)
            read -p "Enter directory name to backup: " directory
            mkdir BackUp
            cp -r $directory BackUp
-           ls BackUp;;
+           ls BackUp;; #backs up a directory including all of its files
         6)
             echo -e "Exiting Script";
-            break;;
+            break;; #exits the script, ends the program
+        *)
+            echo "Invalid entry, please pick an option from the menu provided";;
     esac
 done
