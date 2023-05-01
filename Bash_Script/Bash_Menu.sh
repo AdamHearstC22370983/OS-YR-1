8e4d7be888925ae0cff916f8499541e53740b28b
AdamHearstC22370983 <124101593+AdamHearstC22370983@users.noreply.github.com>
2023-05-01 16:16:45 +0000

    Beta Stage of Bash Script (program runs and loops)

--- Bash_Script/Bash_Menu.sh
+++ Bash_Script/Bash_Menu.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 #creating a bash script to allow the user to execute multiple commands
-#create variables before main body
-
-
-
-
+while true
 do
-    select option in "List Files" "Show free disk space" "Show system path" "Display command history" "Backup Files" "Exit"
+    echo "1. List all files in current directory"
+    echo "2. Show free disk space"
+    echo "3. Show system path"
+    echo "4. Display command history"
+    echo "5. Backup Files"
+    echo "6. Exit"
+    read -p "Please choose from the following options: " option
 
     case $option in
-        "List Files")
-            echo -e "Listing all files in current directory";
+        1)
+            echo -e "Listing all files in current directory"
             ls;;
-        "Show free disk space")
-            echo -e "Showing free disk space";
+        2)
+            echo -e "Showing free disk space"
             df -h;;
-        "Show system path")
+        3)
             echo -e "System Path: $PATH";;
-        "Display command history")
-            echo -e "Displaying command history";
+        4)
+            echo -e "Displaying command history"
             cat ~/.bash_history;;
-        "Backup Files")
+        5)
            read -p "Enter directory name to backup: " directory
            mkdir BackUp
            cp -r $directory BackUp
            ls BackUp;;
-        "Exit")
+        6)
             echo -e "Exiting Script";
             break;;
     esac
